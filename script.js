ab48c25906f2f26ae2f47675544e0d27cf0dd9f0
Kinghuncho <jonyweka@gmail.com>
2026-07-27 22:53:30 +0300

    Initial portfolio website

---
+++ script.js
@@ -0,0 +1,127 @@
+const roles = [
+    "AI Data Annotator",
+    "Data Entry Specialist",
+    "Virtual Assistant",
+    "Internet Researcher"
+];
+
+let roleIndex = 0;
+let charIndex = 0;
+
+const typingElement = document.getElementById("typing");
+
+function type() {
+
+    if (charIndex < roles[roleIndex].length) {
+
+        typingElement.textContent += roles[roleIndex].charAt(charIndex);
+
+        charIndex++;
+
+        setTimeout(type, 80);
+
+    } else {
+
+        setTimeout(erase, 1500);
+
+    }
+
+}
+
+function erase() {
+
+    if (charIndex > 0) {
+
+        typingElement.textContent =
+            roles[roleIndex].substring(0, charIndex - 1);
+
+        charIndex--;
+
+        setTimeout(erase, 40);
+
+    } else {
+
+        roleIndex++;
+
+        if (roleIndex >= roles.length) {
+
+            roleIndex = 0;
+
+        }
+
+        setTimeout(type, 300);
+
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    type();
+
+    const cards = document.querySelectorAll(
+        ".skill-card, .project-card, .education-card"
+    );
+
+    const observer = new IntersectionObserver((entries) => {
+
+        entries.forEach(entry => {
+
+            if (entry.isIntersecting) {
+
+                entry.target.classList.add("show");
+
+            }
+
+        });
+
+    }, {
+        threshold: 0.2
+    });
+
+    cards.forEach(card => {
+
+        observer.observe(card);
+
+    });
+
+});
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+
+        topBtn.style.display = "block";
+
+    }else{
+
+        topBtn.style.display = "none";
+
+    }
+
+});
+
+topBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top:0,
+        behavior:"smooth"
+
+    });
+
+});
+window.addEventListener("load", () => {
+
+    const loader = document.getElementById("loader");
+
+    loader.style.opacity = "0";
+
+    setTimeout(() => {
+
+        loader.style.display = "none";
+
+    }, 600);
+
+});
